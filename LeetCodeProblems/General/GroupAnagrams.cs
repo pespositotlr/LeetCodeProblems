4998f1753ba79ce3326675fb4779e103d8857f0e
Peter Esposito <pespositotlr@gmail.com>
2023-10-18 00:05:01 -0400

    Add GroupAnagrams and TopKFreuqent

---
+++ LeetCodeProblems/General/GroupAnagrams.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeProblems.General
+{
+    internal class GroupAnagrams
+    {
+        public static bool TestGroupAnagrams()
+        {
+            string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+            var result = GroupAnagramsClass.GroupAnagrams(strs);
+            List<IList<string>> expected = new List<IList<string>>();
+            expected.Add(new List<string> { "eat", "tea", "ate" });
+            expected.Add(new List<string> { "tan", "nat" });
+            expected.Add(new List<string> { "bat" });
+            for (int i = 0; i < result.Count; i++)
+            {
+                return expected[i].ToList().SequenceEqual(result[i].ToList());
+            }
+
+            return false;
+        }
+
+        private class GroupAnagramsClass
+        {
+            public static IList<IList<string>> GroupAnagrams(string[] strs)
+            {
+                Dictionary<char[], List<string>> dict = new Dictionary<char[], List<string>>(new CharComparer());
+                foreach (var str in strs)
+                {
+                    var key = str.ToCharArray(); //Get a character array of each input string
+                    Array.Sort(key); //Sort them alphabetically so eat/tea both become aet
+                    if (!dict.ContainsKey(key))
+                    {
+                        dict[key] = new List<string>(); //Add an empty list if it doesn't contain that key
+                    }
+                    dict[key].Add(str); //Add that string to the internal list in the dictionary for that sorted char array
+                }
+
+                List<IList<string>> res = new List<IList<string>>(dict.Values.ToList());
+                return res;
+            }
+        }
+
+        //Alternative is to create a 26-index inner-array
+
+        public class CharComparer : IEqualityComparer<char[]>
+        {
+            public bool Equals(char[] x, char[] y)
+            {
+                if (x == null || y == null)
+                {
+                    return false;
+                }
+
+                if (x.Length != y.Length)
+                {
+                    return false;
+                }
+
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (x[i] != y[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            public int GetHashCode(char[] obj)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ LeetCodeProblems/General/TopKFrequent.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeProblems.General
+{
+    //https://leetcode.com/problems/top-k-frequent-elements/
+    public class TopKFrequent
+    {
+        //Ex: 3, 3, 3, 2, 2, 1
+        public static IList<int> GetTopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> numberToCount = new Dictionary<int, int>();
+
+            //The max number of occurances has to be the length of numbs
+            //It's possible to have ties (But it doesn't say what to do then)
+
+            //we count how many times each number appears
+            //Key = Frequency
+            //Value = Numerical Value
+            foreach (var num in nums)
+            {
+                numberToCount.TryGetValue(num, out var temp);
+                numberToCount[num] = temp + 1;
+            }
+
+            List<int>[] bucket = new List<int>[nums.Length + 1];
+
+            //we allocate an array in the size of the original list of numbers
+            //we iterate all of the numbers and for add each number to the index in the array
+            //the index represents how many times that number appeared
+            //
+            //    0 times -> none
+            //    1 times -> number 3
+            //    2 times -> number 2
+            //    3 times -> number 1
+            //    4 times -> none
+            //    5 times -> none
+            foreach (var key in numberToCount.Keys)
+            {
+                int frequency = numberToCount[key];
+                if (bucket[frequency] == null)
+                {
+                    bucket[frequency] = new List<int>(); //Put a blank list by default
+                }
+                bucket[frequency].Add(key);
+            }
+
+            List<int> result = new List<int>();
+            // we iterate the list bucket in reverse until the number of items in the result
+            // list equals k, because we iterate in reverse we get the biggest numbers
+            for (int pos = bucket.Length - 1; pos >= 0 && result.Count < k; pos--)
+            {
+                if (bucket[pos] != null)
+                {
+                    result.AddRange(bucket[pos]);
+                }
+            }
+
+            return result;
+        }
+
+        public IList<int> TopKFrequentLinqQuery(int[] nums, int k)
+        {
+            var answer = (from int n in nums
+                          group n by n into g
+                          orderby g.Count() descending
+                          select g.Key).Take(k).ToList();
+            return answer;
+        }
+    }
+}
