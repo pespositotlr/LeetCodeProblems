6dc0fbbdcbfcc0496939733fa777865412dee14c
Peter Esposito <pespositotlr@gmail.com>
2025-02-08 01:23:07 -0500

    Move CombinationSum to proper folder

--- LeetCodeProblems/General/CombinationSum.cs
+++ LeetCodeProblems/General/CombinationSum.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace LeetCodeProblems
+namespace LeetCodeProblems.General
 {
     /// <summary>
     /// https://leetcode.com/problems/combination-sum/
